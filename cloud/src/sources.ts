f2636d241c4e068c190ebb09519c5585850a584b
tarematsu <yuukiar0202@gmail.com>
2026-07-17 23:39:48 +0900

    Remove Cloudflare Images binding from worker types

--- cloud/src/sources.ts
+++ cloud/src/sources.ts
@@ -5,7 +5,6 @@ import { fetchStationhead } from "./spotify_source";
 
 export interface Env {
   DB: D1Database;
-  IMAGES: ImagesBinding;
   UPDATE_BUCKET?: R2Bucket;
   HOMEPANEL_INGEST_SECRET?: string;
   HOMEPANEL_DEVICE_TOKENS?: string;
