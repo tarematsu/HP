5dc0e5d741558dfaa9a7a18ed8e82238f2cb3d85
tarematsu <yuukiar0202@gmail.com>
2026-07-19 06:13:58 +0900

    Reduce repeated native JSON property lookups (#140)

--- native/src/json_helpers.h
+++ native/src/json_helpers.h
@@ -9,9 +9,8 @@ using winrt::Windows::Data::Json::JsonValueType;
 
 inline JsonObject Object(const JsonObject& parent, const wchar_t* name) {
   try {
-    if (parent.HasKey(name) && parent.GetNamedValue(name).ValueType() == JsonValueType::Object) {
-      return parent.GetNamedObject(name);
-    }
+    const auto value = parent.GetNamedValue(name);
+    if (value.ValueType() == JsonValueType::Object) return value.GetObject();
   } catch (...) {
   }
   return JsonObject{};
@@ -19,9 +18,8 @@ inline JsonObject Object(const JsonObject& parent, const wchar_t* name) {
 
 inline JsonArray Array(const JsonObject& parent, const wchar_t* name) {
   try {
-    if (parent.HasKey(name) && parent.GetNamedValue(name).ValueType() == JsonValueType::Array) {
-      return parent.GetNamedArray(name);
-    }
+    const auto value = parent.GetNamedValue(name);
+    if (value.ValueType() == JsonValueType::Array) return value.GetArray();
   } catch (...) {
   }
   return JsonArray{};
@@ -29,9 +27,8 @@ inline JsonArray Array(const JsonObject& parent, const wchar_t* name) {
 
 inline std::wstring Text(const JsonObject& object, const wchar_t* name, const std::wstring& fallback = {}) {
   try {
-    if (object.HasKey(name) && object.GetNamedValue(name).ValueType() == JsonValueType::String) {
-      return object.GetNamedString(name).c_str();
-    }
+    const auto value = object.GetNamedValue(name);
+    if (value.ValueType() == JsonValueType::String) return value.GetString().c_str();
   } catch (...) {
   }
   return fallback;
@@ -39,9 +36,8 @@ inline std::wstring Text(const JsonObject& object, const wchar_t* name, const st
 
 inline double Number(const JsonObject& object, const wchar_t* name, double fallback = 0) {
   try {
-    if (object.HasKey(name) && object.GetNamedValue(name).ValueType() == JsonValueType::Number) {
-      return object.GetNamedNumber(name);
-    }
+    const auto value = object.GetNamedValue(name);
+    if (value.ValueType() == JsonValueType::Number) return value.GetNumber();
   } catch (...) {
   }
   return fallback;
@@ -49,9 +45,8 @@ inline double Number(const JsonObject& object, const wchar_t* name, double fallb
 
 inline bool Boolean(const JsonObject& object, const wchar_t* name, bool fallback = false) {
   try {
-    if (object.HasKey(name) && object.GetNamedValue(name).ValueType() == JsonValueType::Boolean) {
-      return object.GetNamedBoolean(name);
-    }
+    const auto value = object.GetNamedValue(name);
+    if (value.ValueType() == JsonValueType::Boolean) return value.GetBoolean();
   } catch (...) {
   }
   return fallback;
@@ -59,9 +54,9 @@ inline bool Boolean(const JsonObject& object, const wchar_t* name, bool fallback
 
 inline std::wstring Stringify(const JsonObject& object, const wchar_t* name) {
   try {
-    if (object.HasKey(name)) return object.GetNamedValue(name).Stringify().c_str();
+    return object.GetNamedValue(name).Stringify().c_str();
   } catch (...) {
   }
   return {};
 }
-}
+}  // namespace hp::json
