cc7fa1b43db142271fab65458073279f3993964b
tarematsu <yuukiar0202@gmail.com>
2026-07-26 07:34:04 +0900

    Attribute recovery Queue messages

--- worker/src/buddies-recovery-entry.js
+++ worker/src/buddies-recovery-entry.js
@@ -1,5 +1,24 @@
-export {
+import recoveryApp, {
   BUDDIES_RECOVERY_QUEUE_NAMES,
   runBuddiesRecoveryQueue,
 } from './buddies-recovery-core.js';
-export { default } from './buddies-recovery-core.js';
+import { queueAttributedEnv } from './queue-attribution.js';
+
+export {
+  BUDDIES_RECOVERY_QUEUE_NAMES,
+  runBuddiesRecoveryQueue,
+};
+
+export function runAttributedBuddiesRecoveryQueue(batch, env, ctx, dependencies) {
+  return runBuddiesRecoveryQueue(
+    batch,
+    queueAttributedEnv(env, 'sh-buddies-recovery'),
+    ctx,
+    dependencies,
+  );
+}
+
+export default {
+  ...recoveryApp,
+  queue: runAttributedBuddiesRecoveryQueue,
+};
