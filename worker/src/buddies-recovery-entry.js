aaacca0fa85cdee15f8f0ca15d9bdb001bb969a5
tarematsu <yuukiar0202@gmail.com>
2026-07-26 04:04:48 +0900

    Route recovery entry through its dedicated core

--- worker/src/buddies-recovery-entry.js
+++ worker/src/buddies-recovery-entry.js
@@ -1,5 +1,5 @@
-import { runBuddiesCollectorQueue } from './buddies-collector-core.js';
-
-export default {
-  queue: runBuddiesCollectorQueue,
-};
+export {
+  BUDDIES_RECOVERY_QUEUE_NAMES,
+  runBuddiesRecoveryQueue,
+} from './buddies-recovery-core.js';
+export { default } from './buddies-recovery-core.js';
