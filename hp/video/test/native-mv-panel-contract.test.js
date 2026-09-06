dd6b482a8fb05864b7eb2d9f8f8fcdf93982fdd8
tarematsu <yuukiar0202@gmail.com>
2026-09-06 15:15:53 +0900

    test: read native media contract from base include

--- hp/video/test/native-mv-panel-contract.test.js
+++ hp/video/test/native-mv-panel-contract.test.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'node:fs';
 import test from 'node:test';
 
 const mediaPanel = readFileSync(
-  new URL('../../native/src/renderer_panels/media_section.inc', import.meta.url),
+  new URL('../../native/src/renderer_panels/media_section_base.inc', import.meta.url),
   'utf8',
 );
 const nativeWindows = readFileSync(
