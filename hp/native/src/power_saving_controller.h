2d7321ee3240426765549f886f6cb6994001ccf3
tarematsu <yuukiar0202@gmail.com>
2026-09-10 08:04:23 +0900

    Replace power-saving thread hook with targeted subclassing

--- hp/native/src/power_saving_controller.h
+++ hp/native/src/power_saving_controller.h
@@ -13,6 +13,7 @@ class PowerSavingController {
 
   void InstallForCurrentThread();
   void Uninstall() noexcept;
+  static void AttachCurrent(HWND parent);
 
  private:
   struct BrightnessState;
@@ -21,12 +22,17 @@ class PowerSavingController {
   static constexpr UINT_PTR kMvStartupPassTimer = 2;
   static constexpr UINT kRaiseOverlayMessage = WM_APP + 1;
 
-  static LRESULT CALLBACK CallWndProc(int code, WPARAM wParam, LPARAM lParam);
+  static LRESULT CALLBACK ParentWndProc(
+      HWND window, UINT message, WPARAM wParam, LPARAM lParam);
+  static LRESULT CALLBACK MvWndProc(
+      HWND window, UINT message, WPARAM wParam, LPARAM lParam);
   static LRESULT CALLBACK OverlayWndProc(
       HWND window, UINT message, WPARAM wParam, LPARAM lParam);
 
-  void ObserveMessage(const CWPSTRUCT& message);
+  void ObserveParentMessage(UINT message, WPARAM wParam, LPARAM lParam);
   void Attach(HWND parent);
+  void AttachMvWindow(HWND window) noexcept;
+  void DetachMvWindow() noexcept;
   void Detach() noexcept;
   void EnsureOverlay();
   void CheckSchedule(bool force = false);
@@ -44,9 +50,11 @@ class PowerSavingController {
   RECT LocalPowerButtonRect() const;
   RECT LocalMuteButtonRect() const;
 
-  HHOOK hook_ = nullptr;
   HWND parent_ = nullptr;
   HWND overlay_ = nullptr;
+  HWND mvWindow_ = nullptr;
+  WNDPROC parentWndProc_ = nullptr;
+  WNDPROC mvWndProc_ = nullptr;
   bool powerSaving_ = false;
   bool mediaMuted_ = false;
   bool mvStartupInputPass_ = false;
