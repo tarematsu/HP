e655287774d83bad9e68ab3eb653910b1efee5c5
tarematsu <yuukiar0202@gmail.com>
2026-09-12 01:07:44 +0900

    Add inline TALKABOUT rotation state

--- hp/native/src/spotify_webviews.h
+++ hp/native/src/spotify_webviews.h
@@ -224,6 +224,7 @@ class SpotifyWebViews final {
   bool started_ = false;
   bool robustSchedulerStarted_ = false;
   bool networkBlocked_ = false;
+  bool inlineTalkAboutRotation_ = false;
   bool podcastScheduleLoaded_ = false;
 };
 
