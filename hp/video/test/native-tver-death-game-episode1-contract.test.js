7beded6a6a2102bbc6c3eee50663019eca06519f
tarematsu <yuukiar0202@gmail.com>
2026-09-11 11:48:50 +0900

    TVer series個別契約を単一cloud queueへ更新

--- hp/video/test/native-tver-death-game-episode1-contract.test.js
+++ hp/video/test/native-tver-death-game-episode1-contract.test.js
@@ -8,7 +8,8 @@ const refresh = readFileSync(
   new URL('../../native/src/renderer_panels/media_tver_cloud_queue_refresh.inc', import.meta.url), 'utf8');
 
 test('TVer playback does not special-case a series page or recommended episode', () => {
-  assert.match(episode, /__homePanelTverEpisodeQueue:/);
+  assert.match(episode, /episodeQueueKey = '__homePanelTverEpisodeQueue'/);
+  assert.doesNotMatch(episode, /__homePanelTverEpisodeQueue:/);
   assert.match(episode, /location\.replace\(queue\.hrefs\[nextIndex\]\)/);
   assert.doesNotMatch(episode, /deathGameSeriesPath|findSeriesEpisodeContainer|おすすめ/);
   assert.match(refresh, /for \(const href of fresh\)/);
