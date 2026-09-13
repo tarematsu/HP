c3aa290b05e3bad7966a3f086fb29615cef45927
tarematsu <yuukiar0202@gmail.com>
2026-09-13 21:36:37 +0900

    Define Spotify telemetry coalesce interval

--- hp/native/src/spotify_playback_telemetry.h
+++ hp/native/src/spotify_playback_telemetry.h
@@ -4,6 +4,7 @@
 namespace hp {
 
 inline constexpr UINT kSpotifyPlaybackTelemetryChangedMessage = WM_APP + 23;
+inline constexpr ULONGLONG kSpotifyPlaybackTelemetryMinIntervalMs = 15ULL * 1000ULL;
 
 std::string CurrentSpotifyPlaybackTelemetryJson() noexcept;
 
