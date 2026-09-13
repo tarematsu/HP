870ee3b170cfa43a1dce957c8e459b1f1a896bd7
tarematsu <yuukiar0202@gmail.com>
2026-09-13 10:11:55 +0900

    Remove native Spotify repeat control state

--- hp/native/src/spotify_webviews.h
+++ hp/native/src/spotify_webviews.h
@@ -50,11 +50,6 @@ class SpotifyWebViews final {
     Completed,
   };
 
-  enum class PlaybackModeGuard : unsigned char {
-    Shuffle,
-    Repeat,
-  };
-
   struct ManagedTrack {
     std::wstring title;
     std::wstring url;
@@ -119,7 +114,6 @@ class SpotifyWebViews final {
     bool controllerCreating = false;
     bool reconcileInFlight = false;
     bool shuffleOffVerified = false;
-    bool repeatOffVerified = false;
     bool playerPage = false;
     bool loginPage = false;
     bool timedObserverReady = false;
@@ -182,7 +176,7 @@ class SpotifyWebViews final {
   MusicTargetDescriptor ResolveMusicTarget(const Slot& slot) const noexcept;
   bool SlotMatchesMusicTarget(const Slot& slot) const noexcept;
   void NavigateMusicTarget(Slot& slot) noexcept;
-  bool EnsurePlaybackModeOff(Slot& slot, PlaybackModeGuard mode) noexcept;
+  bool EnsureShuffleOff(Slot& slot) noexcept;
   void ReconcileMusicTarget(Slot& slot) noexcept;
   void NavigateActiveTimedSlot(Slot& slot) noexcept;
   void ReconcileActiveTimedSlot(Slot& slot) noexcept;
