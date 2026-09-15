d79d20bf8c59a2b0188328bf3bcb9687e2238ace
tarematsu <yuukiar0202@gmail.com>
2026-09-15 21:45:52 +0900

    Expose deferred Spotify startup

--- hp/native/src/web_renderer.h
+++ hp/native/src/web_renderer.h
@@ -120,6 +120,7 @@ class Renderer {
   ~Renderer();
   static void SetGlobalPowerSavingMode(bool enabled);
   void Initialize();
+  void StartSpotify();
   void Resize(int width, int height);
   void SetBounds(const RECT& bounds);
   void SetVisible(bool visible);
@@ -191,7 +192,6 @@ class Renderer {
     PAINTSTRUCT paint{};
     HDC paintDc = nullptr;
     HDC dc = nullptr;
-    HGDIOBJ previousBitmap = nullptr;
     RECT bounds{};
     RECT dirty{};
   };
