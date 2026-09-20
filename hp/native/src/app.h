6e9843beb53e125159306c39dd8b2bd00f4803f1
tarematsu <yuukiar0202@gmail.com>
2026-09-20 12:08:38 +0900

    Move Stationhead fallback policy out of App header

--- hp/native/src/app.h
+++ hp/native/src/app.h
@@ -5,55 +5,12 @@
 #include "logger.h"
 #include "render_state.h"
 #include "sensors.h"
+#include "stationhead_fallback_revision_gate.h"
 #include "update_client.h"
 
 namespace hp {
 
 class Renderer;
-
-inline int64_t operator+(
-    const MonotonicElapsedTimestamp& timestamp, int intervalMs) noexcept {
-  return timestamp + static_cast<int64_t>(intervalMs);
-}
-
-class StationheadFallbackRevisionGate {
- public:
-  StationheadFallbackRevisionGate& operator=(uint64_t healthyRevision) noexcept {
-    if (healthyRevision == 0) {
-      Reset();
-    } else {
-      Arm(healthyRevision);
-    }
-    return *this;
-  }
-
-  friend bool operator>(
-      uint64_t healthyRevision,
-      const StationheadFallbackRevisionGate& gate) noexcept {
-    return gate.CanRelease(healthyRevision);
-  }
-
-  void Arm(uint64_t healthyRevision) noexcept {
-    baselineHealthyRevision_ = healthyRevision;
-    startedAt_ = UnixMillis();
-  }
-
-  void Reset() noexcept {
-    baselineHealthyRevision_ = 0;
-    startedAt_ = 0;
-  }
-
-  [[nodiscard]] bool CanRelease(uint64_t healthyRevision) const noexcept {
-    return startedAt_.Active() && healthyRevision != 0 &&
-        startedAt_.ElapsedMilliseconds() >=
-            kStationheadFallbackMinimumDwellMs &&
-        healthyRevision > baselineHealthyRevision_;
-  }
-
- private:
-  uint64_t baselineHealthyRevision_ = 0;
-  MonotonicElapsedTimestamp startedAt_;
-};
 
 class App {
  public:
