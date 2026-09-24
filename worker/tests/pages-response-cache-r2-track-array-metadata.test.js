602dc688504c28d55167aaeb07e2910f56d77e90
tarematsu <yuukiar0202@gmail.com>
2026-09-24 09:35:15 +0900

    Guard array track history R2 metadata

---
+++ worker/tests/pages-response-cache-r2-track-array-metadata.test.js
@@ -0,0 +1,9 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+
+import { loadMaterializedR2Response } from '../src/pages-response-r2.js';
+
+test('array track history metadata is treated as a miss', async () => {
+  const response = await loadMaterializedR2Response({ get: async () => ({ body: '{}', customMetadata: [] }) }, 'track-history');
+  assert.equal(response, null);
+});
