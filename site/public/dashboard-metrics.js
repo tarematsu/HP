c4112ff453d7417899fd4b03a69dbfd5ed66b938
tarematsu <yuukiar0202@gmail.com>
2026-09-24 09:23:44 +0900

    用語統一モジュールを読み込み

--- site/public/dashboard-metrics.js
+++ site/public/dashboard-metrics.js
@@ -1,6 +1,7 @@
 import './history/history-global-fixes.js';
 import './pages-ui-tweaks.js?v=20260921.1';
 import './dashboard-header.js?v=20260923.4';
+import './pages-terminology.js?v=20260924.1';
 import './unofficial-listening-parties.js?v=20260923.2';
 import './official-listening-party-copy.js?v=20260923.1';
 import './played-tracks-shell.js?v=20260924.3';
